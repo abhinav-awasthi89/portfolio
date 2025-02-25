0ad7cff65bae43d555241075ac0b519d73da9279
Abhinav Awasthi <2k22.csai.2211953@gmail.com>
2025-02-25 14:42:08 +0530

    Portfolio files

---
+++ assets/main.js
@@ -0,0 +1,41 @@
+/*==================== ACCORDION SKILLS ====================*/
+
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
+
+/*==================== Path TABS ====================*/
+
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("path__active");
+    });
+    target.classList.add("path__active");
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("path__active");
+    });
+    tab.classList.add("path__active");
+  });
+});
+
